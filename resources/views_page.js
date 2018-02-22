d688372ac5b5f4bf4af9bcd496ea4310b1dd5a3e
Nicolas Badia <badia.nicolas@gmail.com>
2018-02-22 15:03:54 +0100

    Moves WellViews demo in it's own file

--- resources/views_page.js
+++ resources/views_page.js
@@ -266,23 +266,7 @@ Showcase.viewsPage = SC.Page.create({
 
   webViews: Showcase.webViews,
 
-  wellViews: SC.ScrollView.design({
-    contentView: Showcase.ViewsListView.design({
-      content: [
-        Showcase.ViewsItemContent.create({
-          title: 'Regular',
-          example: "SC.WellView.extend({  layout: { left: 20, right: 20, top: 20, bottom: 20 },  nowShowing: 'blueSampleView',  blueSampleView: SC.LabelView.extend({  classNames:['blue-sample-view'],  value: 'Blue View'  }),  redSampleView: SC.LabelView.extend({  classNames:['red-sample-view'],  value: 'Red View'  })  });",
-          supportAction: function() {
-            var containerView = this.getPath('parentView.exampleBox.contentView');
-            if (containerView.get('nowShowing') === 'blueSampleView') containerView.set('nowShowing', 'redSampleView');
-            else containerView.set('nowShowing', 'blueSampleView');
-          },
-          supportTitle: 'Toggle nowShowing'
-        })
-      ],
-      exampleHeight: 315
-    })
-  }),
+  wellViews: Showcase.wellViews,
 
   workspaceViews: Showcase.workspaceViews
 
